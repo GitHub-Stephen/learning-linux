142790603aed86b8c47f44bb1214ba5b0bab3659
zhuweicong <308054005@qq.com>
2024-01-09 06:42:40 +0800

    add: vitepress config

---
+++ .vitepress/config.mjs
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Learning Linux",
+  description: "About Linux BaseKnowlege",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: '返回首页', link: '/' },
+    ],
+
+    sidebar: [
+      {
+        text: '目录与权限',
+        items: [
+          { text: '文件系统', link: '文件系统' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
